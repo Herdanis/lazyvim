b657671b8c5618da07f15355a0d4fe1feb5daf90
Herdanis <herdanis.pradana@gmail.com>
2024-03-28 09:33:36 +0700

    init commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+--
+-- map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+-- map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+map("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
