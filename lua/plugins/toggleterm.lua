b2cae90dc37daf8ec9f32048cc9dbccf1247828d
Herdanis <herdanis.pradana@gmail.com>
2024-08-01 16:43:12 +0700

    add: tmux integration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
       require("toggleterm").setup({
-        open_mapping = [[<c-\>]],
+        open_mapping = [[<A-\>]],
         shade_terminals = false,
         -- add --login so ~/.zprofile is loaded
         -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
@@ -11,7 +11,7 @@ return {
       })
     end,
     keys = {
-      { [[<C-\>]] },
+      { [[<A-\>]] },
       { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
       {
         "<leader>td",
